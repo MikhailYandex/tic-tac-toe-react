9e8de39a0013e75bb01ecd324f4803049ef7a55d
Mikhail Ponomarenko <mishanya-101@yandex.ru>
2023-04-21 20:13:54 +0400

    feat: all functionality was done

--- src/App.js
+++ src/App.js
@@ -1,73 +1,77 @@
 import { useState } from "react";
 
 const App = () => {
-	const emptyBoard = Array(9).fill(null);
-	const [board, setBoard] = useState(emptyBoard);
-	const [xIsNext, setxIsNext] = useState(true);
+  const emptyBoard = Array(9).fill(null);
+  const [board, setBoard] = useState(emptyBoard);
+  const [xIsNext, setxIsNext] = useState(true);
 
-	const calculateWinner = (board) => {
-		const lines = [
-			[0, 1, 2],
-			[3, 4, 5],
-			[6, 7, 8],
-			[0, 3, 6],
-			[1, 4, 7],
-			[2, 5, 8],
-			[0, 4, 8],
-			[2, 4, 6],
-		];
-		for (let i = 0; i < lines.length; i++) {
-			const [a, b, c] = lines[i];
-			if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-				return board[a];
-			}
-		}
-		return null;
-	}
+  const calculateWinner = (board) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        return board[a];
+      }
+    }
+    return null;
+  };
 
-  const winner = calculateWinner(board)
+  const winner = calculateWinner(board);
 
-	const handleClick = (index) => {
-		// вернуть если выиграл, либо всё заполнено
-		if (winner || board[index]) return;
+  const handleClick = (index) => {
+    // вернуть если выиграл, либо всё заполнено
+    if (winner || board[index]) return;
 
-  	// выбор квадрата
-		const boardCopy = [...board]
-		boardCopy[index] = xIsNext ? 'X' : 'O'
-		setBoard(boardCopy)
-		setxIsNext(!xIsNext);
-	}
+    // выбор квадрата
+    const boardCopy = [...board];
+    boardCopy[index] = xIsNext ? "X" : "O";
+    setBoard(boardCopy);
+    setxIsNext(!xIsNext);
+  };
 
-	const renderStatus = () => {
+  const renderStatus = () => {
     if (winner) {
       return `Winner: ${winner}`;
-    } else if (board.every(item => item !== null)) {
-      return 'Draw!';
+    } else if (board.every((item) => item !== null)) {
+      return "Draw!";
     } else {
-      return `Next player: ${xIsNext ? 'X' : 'O'}`;
+      return `Next player: ${xIsNext ? "X" : "O"}`;
     }
-  }
+  };
 
-	const resetGame = () => {
+  const resetGame = () => {
     setBoard(emptyBoard);
-  }
+  };
 
   return (
-		<>
-			<h1> Tic Tac Toe Game </h1>
-			<div className="board">
-				{board.map((item, index) => (
-					<button className='item' key={index} onClick={() => handleClick(index)}>
-					{item}
-				</button>
-				))}
-			</div>
-			<div className="status">{renderStatus()}</div>
-			<button className="reset-button" onClick={resetGame}>
+    <>
+      <h1> Tic Tac Toe Game </h1>
+      <div className="board">
+        {board.map((item, index) => (
+          <button
+            className="item"
+            key={index}
+            onClick={() => handleClick(index)}
+          >
+            {item}
+          </button>
+        ))}
+      </div>
+      <div className="status">{renderStatus()}</div>
+      <button className="reset-button" onClick={resetGame}>
         Restart Game
       </button>
-		</>
+    </>
   );
-}
+};
 
 export default App;
