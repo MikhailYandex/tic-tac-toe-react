a6a98f57cbbf324cc9c0d6ee70eebe18b5c0e543
Mikhail Ponomarenko <mishanya-101@yandex.ru>
2023-04-21 19:37:13 +0400

    feat: tic-tac-toe was done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+	const emptyBoard = Array(9).fill(null);
+	const [board, setBoard] = useState(emptyBoard);
+	const [xIsNext, setxIsNext] = useState(true);
+
+	const calculateWinner = (board) => {
+		const lines = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
+			[0, 4, 8],
+			[2, 4, 6],
+		];
+		for (let i = 0; i < lines.length; i++) {
+			const [a, b, c] = lines[i];
+			if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+				return board[a];
+			}
+		}
+		return null;
+	}
+
+  const winner = calculateWinner(board)
+
+	const handleClick = (index) => {
+		// вернуть если выиграл, либо всё заполнено
+		if (winner || board[index]) return;
+
+  	// выбор квадрата
+		const boardCopy = [...board]
+		boardCopy[index] = xIsNext ? 'X' : 'O'
+		setBoard(boardCopy)
+		setxIsNext(!xIsNext);
+	}
+
+	const renderStatus = () => {
+    if (winner) {
+      return `Winner: ${winner}`;
+    } else if (board.every(item => item !== null)) {
+      return 'Draw!';
+    } else {
+      return `Next player: ${xIsNext ? 'X' : 'O'}`;
+    }
+  }
+
+	const resetGame = () => {
+    setBoard(emptyBoard);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+		<>
+			<h1> Tic Tac Toe Game </h1>
+			<div className="board">
+				{board.map((item, index) => (
+					<button className='item' key={index} onClick={() => handleClick(index)}>
+					{item}
+				</button>
+				))}
+			</div>
+			<div className="status">{renderStatus()}</div>
+			<button className="reset-button" onClick={resetGame}>
+        Restart Game
+      </button>
+		</>
   );
 }
 
